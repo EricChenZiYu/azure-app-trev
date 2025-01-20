617d22cf029e3a766f9dfbc0a00c846907f44526
EricChenZiYu <ziyucrc@gmail.com>
2025-01-20 16:54:50 +0800

    feat:init

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_trev.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
